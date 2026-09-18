9ad53149a442e03e15422499ded801c34d888f7c
kiburileonardmwenda-star <kiburileonardmwenda@gmail.com>
2026-09-18 11:34:28 +0300

    Add files via upload

---
+++ mwenda kiburi Leonard .c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+/*
+Name:leonard mwenda kiburi 
+Reg:CT100/G/30751
+Description :units for semester1
+
+*/
+int main()
+{
+    printf("mathematics for science");
+    printf("analogue electronics");
+    printf("introduction to computer applications");
+    printf("esscentials of accounting");
+    printf("computer systems and organizations");
+    printf("introduction to programming and algorithms");
+    printf("communication and information literacy skills");
+    printf("fundamentals of digital technology and artificial");
+    
+    
+    return 0;
+}
